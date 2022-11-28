f0703fe5f75701baf6cdc55f334246cdb136d4f5
Haman692 <haman692@yandex.ru>
2022-11-29 05:37:22 +0700

    task019

---
+++ task_019.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19 Запишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int value = numberA;
+int NumberB = 0;
+for (int i = 0; i < 5; i++)
+{
+    int remaind = value % 10;
+    NumberB = NumberB * 10 + remaind;
+    value = value / 10;
+}
+if (9999 < numberA && numberA < 100000)
+{
+    if (numberA == NumberB)
+        Console.WriteLine($"Число {numberA} являеться палиндромом");
+    else
+        Console.WriteLine($"Число {numberA} не являеться палиндромом");
+}
+else 
+    Console.WriteLine($"Число {numberA} не пятизначное");
---
+++ task_019/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19 Запишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+
+int DateNumber(string str)
+{
+    Console.Write(str);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int ReverseNumber(int value)
+{
+    int count = 0;
+    int number = 0;
+    while(count < 5)
+    {
+        int remaind = value % 10;
+        number = number * 10 + remaind;
+        value = value / 10;
+        count++;
+    }
+    return number;
+}
+
+int numberA = DateNumber("Введите пятизначное число ");
+int numberB = ReverseNumber(numberA);
+
+if (9999 < numberA && numberA < 100000)
+{
+    if(numberA == numberB)
+        Console.WriteLine($"Число {numberA} являеться палиндромом");
+    else
+        Console.WriteLine($"Число {numberA} не являеться палиндромом");
+}
+else 
+    Console.WriteLine($"Число {numberA} не пятизначное");
